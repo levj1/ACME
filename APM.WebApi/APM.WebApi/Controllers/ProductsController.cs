d0a2297563cafadc807a7c804709dc5ff823213d
levj1 <levj1@yahoo.com>
2017-04-05 23:55:39 -0400

    Add product api

---
+++ APM.WebApi/APM.WebApi/Controllers/ProductsController.cs
@@ -0,0 +1,20 @@
+﻿using APM.WebApi.Models;
+using APM.WebApi.Models.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace APM.WebApi.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        public IEnumerable<Product> Get()
+        {
+            var productRepository = new ProductRepository();
+            return productRepository.Retrieve();
+        }
+    }
+}
